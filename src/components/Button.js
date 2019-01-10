cae3650955365de66751d48c8221c0a93d4234c1
ryoji.yamauchi <ryoji_yamauchi@waku-2.com>
2019-01-10 16:36:04 +0900

    Consumerを使う

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,13 +3,21 @@ import LanguageContext from '../context/LanguageContext'
 
 class Button extends React.Component {
   //この名前にしなきゃいけないっぽ　
-  static contextType = LanguageContext
+  //static contextType = LanguageContext
+
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Voyer'
+  }
 
   render() {
     //console.log(this.context)
-    const text =this.context === 'english' ? 'Submit' : 'Voyer'
+    //const text =this.context === 'english' ? 'Submit' : 'Voyer'
     return (
-      <button className="ui button primary">{text}</button>
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {value => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
     )
   }
 }
